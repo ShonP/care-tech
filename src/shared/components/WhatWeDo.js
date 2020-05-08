de0b3a391c9334bdec2127937c7e80765ac8c17a
Shon <shonp@welldone-software.com>
2020-05-09 01:12:43 +0300

    translations

--- src/shared/components/WhatWeDo.js
+++ src/shared/components/WhatWeDo.js
@@ -1,11 +1,13 @@
-import React from "react"
+import React, { useMemo } from "react"
 import _Card from "./Card"
 import styled from "styled-components"
 import education from "../../images/education.svg"
+import communication from "../../images/communication.svg"
 import job from "../../images/job.svg"
+import { useTranslation } from "react-i18next"
 
 const Card = styled(_Card)`
-  height: 25rem;
+  height: 30rem;
   width: 20rem;
   padding: 2rem;
   flex-direction: column;
@@ -15,20 +17,44 @@ const Card = styled(_Card)`
     margin-left: 0;
   }
 `
+
 const Wrapper = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: center;
+  flex-direction: column;
+
   margin-bottom: ${({ theme }) => theme.sizes.siteSpace};
   margin-top: ${({ theme }) => theme.sizes.siteSpace};
 `
 
+const Cards = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
 const Img = styled.img`
-  width: 100%;
+  min-height: 13rem;
+  max-height: 13rem;
   margin-bottom: 2rem;
 `
 
 const Header = styled.h1`
+  font-size: 2.5rem;
+  font-weight: bold;
+  text-align: center;
+  color: ${({ theme }) => theme.colors.primary};
+  margin-bottom: 2rem;
+  &::after {
+    content: ""; /* This is necessary for the pseudo element to work. */
+    display: block; /* This will put the pseudo element on its own line. */
+    margin: 0 auto; /* This will center the border. */
+    width: 15%; /* Change this to whatever width you want. */
+    padding-top: 20px; /* This creates some space between the element and the border. */
+    border-bottom: 1px solid ${({ theme }) => theme.colors.primary}; /* This creates the border. Replace black with whatever color you want. */
+  }
+`
+
+const CardHeader = styled.h1`
   font-size: 1.5rem;
   font-weight: 500;
   text-align: center;
@@ -43,25 +69,35 @@ const SubHeader = styled.p`
   line-height: 1.5rem;
 `
 
-const WhatWeDo = () => (
-  <Wrapper>
-    <Card>
-      <Img src={job} />
-      <Header>מציאת עבודה בקלות</Header>
-      <SubHeader>
-        ניתן לך את כל הטיפים לעבור את ראיון העבודה והמבחן בקלות ובמהירות
-      </SubHeader>
-    </Card>
-    <Card>
-      <Img src={education} />
+const WhatWeDo = () => {
+  const { t } = useTranslation()
 
-      <Header>לימוד יעיל ומהיר</Header>
-      <SubHeader>
-        ניתן לך את הכלים הדרושים להתמודד עם מגוון משימות ובעיות על מנת שתוכל
-        בסופו של דבר להתמודד בעצמך
-      </SubHeader>
-    </Card>
-  </Wrapper>
-)
+  const cards = useMemo(
+    () => [
+      { src: job, header: t("findJob"), subHeader: t("jobTips") },
+      { src: education, header: t("education"), subHeader: t("educationTips") },
+      {
+        src: communication,
+        header: t("communication"),
+        subHeader: t("communicationTips"),
+      },
+    ],
+    []
+  )
+  return (
+    <Wrapper>
+      <Header>{t("whatWeDo")}</Header>
+      <Cards>
+        {cards.map(({ src, header, subHeader }) => (
+          <Card>
+            <Img src={src}></Img>
+            <CardHeader>{header}</CardHeader>
+            <SubHeader>{subHeader}</SubHeader>
+          </Card>
+        ))}
+      </Cards>
+    </Wrapper>
+  )
+}
 
 export default WhatWeDo
--- src/shared/locales/en.js
+++ src/shared/locales/en.js
@@ -9,4 +9,14 @@ export default {
   month: "Month",
   whoWeAreFirstParagraph: `It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).`,
   whoWeAreSecondParagraph: `It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).`,
+  findJob: "Find a job",
+  jobTips:
+    "We will help you understand the best tips to successfuly pass any interview and test",
+  education: "Educate yourself faster and efficiently",
+  educationTips: `
+  We will give you any meaning tools to handle variaty of tasks and issues that you will be able to handle any task yourself`,
+  communication: "Communication and help",
+  communicationTips:
+    "We will invite you to our self made community that will help each other with any challange and task",
+  whatWeDo: "What we do",
 }

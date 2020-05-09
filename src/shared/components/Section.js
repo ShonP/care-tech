797bf90c37671b42b6f0d4b818249dce55d22964
Shon <shonp@welldone-software.com>
2020-05-09 16:22:56 +0300

    fix elements

--- src/shared/components/Section.js
+++ src/shared/components/Section.js
@@ -1,11 +1,18 @@
+import React from "react"
 import styled from "styled-components"
 import { Element } from "react-scroll"
 
-const Section = styled(Element)`
+const Wrapper = styled.div`
   background-color: ${({ theme }) => theme.colors.white};
   display: flex;
   padding: ${({ theme }) => theme.sizes.siteSpace};
   flex-wrap: nowrap;
 `
+
+const Section = ({ name, ...rest }) => (
+  <Element name={name}>
+    <Wrapper {...rest} />
+  </Element>
+)
 
 export default Section
